0939150d2aff3eb2c912bec5cfaf7ea3105f45a0
Daniel Palacios Vivero <dpalacios@estudiodemoda.com.co>
2025-11-04 17:06:30 -0500

    Provisional

--- src/Funcionalidades/Dashboard.ts
+++ src/Funcionalidades/Dashboard.ts
@@ -1,15 +1,23 @@
 import React from "react";
 import { useAuth } from "../auth/authContext";
 import type { TicketsService } from "../Services/Tickets.service";
+import type { GetAllOpts } from "../Models/Commons";
+import type { DateRange } from "../Models/Filtros";
+import { toGraphDateTime, toISODateFlex } from "../utils/Date";
 
 export function useDashboard(TicketsSvc: TicketsService) {
    // const [resolutures, setResolutores] = React.useState<Usuario[]>([])
     const [totalCasos, setTotalCasos] = React.useState<number>(0)
+    //const [totalEnCurso, setTotalencurso] = React.useState<number>(0)
+    //const [totalFueraTiempo, setTotalFueraTiempo] = React.useState<number>(0)
+    //const [totalFinalizados, setTotalFinalizados] = React.useState<number>(0)
     const [loading, setLoading] = React.useState(false);
     const [error, setError] = React.useState<string | null>(null);
+    const today = React.useMemo(() => toISODateFlex(new Date()), []);
+    const [range, setRange] = React.useState<DateRange>({ from: today, to: today });
    // const [filterMode, setFilterMode] = React.useState<FilterMode>("En curso");
-   // const today = React.useMemo(() => toISODateFlex(new Date()), []);
-   // const [range, setRange] = React.useState<DateRange>({ from: today, to: today });
+   
+   
    // const [pageSize, setPageSize] = React.useState<number>(15); // = $top
    // const [pageIndex, setPageIndex] = React.useState<number>(1); // 1-based
    // const [nextLink, setNextLink] = React.useState<string | null>(null);
@@ -17,7 +25,56 @@ export function useDashboard(TicketsSvc: TicketsService) {
    // const [state, setState] = React.useState<RelacionadorState>({TicketRelacionar: null});
     const { account } = useAuth();
 
-    const obtenerTotal = React.useCallback(async (mode: string) => {
+    const buildFilter = React.useCallback((mode: string, changeRange: boolean): GetAllOpts => {
+        const filters: string[] = [];
+    
+        if(mode === "resumen"){
+             filters.push(`fields/CorreoResolutor eq '${account!.username}'`)
+                filters.push(`fields/CorreoResolutor eq '${account!.username}'`);
+
+                const now = new Date();
+                const monthStart = new Date(now.getFullYear(), now.getMonth(), 1, 0, 0, 0, 0);
+                const monthEnd   = new Date(now.getFullYear(), now.getMonth() + 1, 0, 23, 59, 59, 999);
+                const fromIso = toGraphDateTime(monthStart); // -> "YYYY-MM-DDTHH:mm:ssZ"
+                const toIso   = toGraphDateTime(monthEnd);
+                filters.push(`fields/FechaApertura ge ${fromIso}`);
+                filters.push(`fields/FechaApertura le ${toIso}`);
+        }
+                    
+        if(changeRange){
+            filters.push(`fields/FechaApertura ge ${range.from}`);
+            filters.push(`fields/FechaApertura le ${range.to}`);
+        }
+    
+        return {
+          filter: filters.join(" and "),
+        };
+      }, [range.from, range.to,]); 
+
+    const obtenerTotal = React.useCallback(async (mode: string, changeRange:boolean) => {
+        setLoading(true);
+        setError(null);
+        try {
+        // pasa el filtro al servicio (ajusta la firma si tu svc usa otra shape)
+        const casos = await TicketsSvc.getAllPlane(buildFilter(mode, changeRange));
+        //const casosEnCurso = await TicketsSvc.getAllPlane({});
+        const total = Array.isArray(casos)
+            ? casos.length
+            : Array.isArray((casos as any)?.value)
+            ? (casos as any).value.length
+            : 0;
+
+        setTotalCasos(total);
+        } catch (e: any) {
+        setError(e?.message ?? "Error al inicializar escalamiento");
+        } finally {
+        setLoading(false);
+        }
+    },
+    [TicketsSvc, account?.username]
+    );
+
+   /* const obtenerTotalSegmentado = React.useCallback(async (mode: string) => {
         setLoading(true);
         setError(null);
         try {
@@ -44,11 +101,13 @@ export function useDashboard(TicketsSvc: TicketsService) {
         }
     },
     [TicketsSvc, account?.username]
-    );
+    );*/
+
+
 
   return {
-    obtenerTotal, 
-    totalCasos, loading, error
+    obtenerTotal, setRange,
+    totalCasos, error, loading
   };
 }
 
