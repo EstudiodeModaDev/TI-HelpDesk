49bab735cc118044fc361a09eef667109c24142f
Daniel Palacios Vivero <dpalacios@estudiodemoda.com.co>
2025-11-13 08:38:09 -0500

    Campos de nota y encargado en tareas

--- src/Services/Recordatorios.service.ts
+++ src/Services/Recordatorios.service.ts
@@ -39,6 +39,8 @@ export class RecordatoriosService {
         ReportadaporCorreo: f.ReportadaporCorreo,
         Estado: f.Estado,
         Cantidaddediasalarma: f.Cantidaddediasalarma,
+        Nota: f.Nota,
+        
     };
   }
 
